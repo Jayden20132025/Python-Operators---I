4ffbdc15c69f923a44b462aba1039f2494ff81cd
Jayden20132025 <Jaydenmutashubilwa.com>
2025-12-05 20:32:31 +0300

    Activity completed

---
+++ A3.py
@@ -0,0 +1,15 @@
+# tke marks as input from user
+print("Enter Marks Obtained in 4 Subject ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+hindi = int(input("hindi :"))
+
+# Let's calculate the percentage of marks
+sum = math+english+science+hindi
+print("sum of math, english, science and hindi")
+
+perc = (sum/400)*100
+
+print(end="Percentage Mark = ")
+print(perc)
